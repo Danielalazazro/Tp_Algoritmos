14632787011fed4dd87825594e5459cb8d24fddf
Danielalazazro <113920273+Danielalazazro@users.noreply.github.com>
2022-11-07 15:55:53 -0300

    Update celda.h

--- celda.h
+++ celda.h
@@ -4,30 +4,142 @@
 
 #ifndef TP2_CELULAS_H
 #define TP2_CELULAS_H
+#include "Celula.h"
+#include "Lista.h"
 
 
 class Celda {
 private:
-
-    int estadoDeCelda;//estado que epuede variar
+    //Para 2 dimenciones // y para 3 dimensiones es Celda****vecinos
+    Celda****vecinosAdyacentes;
+    Lista<Celda*> * celdasAdyacentes;
+    int estadoDeCelda; //estadoActual que epuede variar
+    Celula* celula;
+    int posicionX;
+    int posicionY;
+    int posicionZ;
 public:
-    int aplicarConportamiento();
+    /*
+     * pre:
+     * post:
+     */
+    Celda();
+    /*
+     * pre:
+     * post:
+     */
+    Celda(int filaIngresa, int columnaIngresada, int profundidadIngresada);
+    /*
+     * pre:
+     * post:
+     */
+    void aplicarConportamiento(int *cantCelNacidasTurno);
+    /*
+     * pre:
+     * post:
+     */
     void aplicarEnvenenamiento();
+    /*
+     * pre:
+     * post:
+     */
     void aplicarContaminacion();
-    void aplicarPortal();
+    /*
+     * pre:
+     * post:
+     */
+    void aplicarPortal(int *cantCelNacidasTurno);
+    /*
+     *pre:
+     *post:
+     */
     void aplicarProcreadora();
+    /*
+     *pre:
+     *post:
+     */
     void  aplicarRadioactividad();
+    /*
+     * pre:-
+     * post: cambia el estadoActual de la tableroCelda
+     */
+    void setCelula(Celula* celulaIngresada);
+    /*
+     * pre:Las coordenadas de vecinos van de  -1 (izquierda) a 1 (horizontal) y de -1 (arriba) a 1 (abajo) en vertical
+     * post:
+     */
+    void asignarVecino(int ancho, int alto, int profundidad, Celda* celVecino);
+    /*
+     * pre: Las coordenadas de vecinos van de  -1 (izquierda) a 1 (horizontal) y de -1 (arriba) a 1 (abajo) en vertical
+     * post: devulve el vecino o vacio.
+     */
+    Celda* obtenerVecino();
+    /*
+     * pre: Las coordenadas de vecinos van de  -1 (izquierda) a 1 (horizontal) y de -1 (arriba) a 1 (abajo) en vertical
+     * post: devulve verdadero o falso en caso de que haya una celula.
+     */
+    bool tieneVecino(int ancho, int alto, int profundidad);
 
+    /*
+     * pre:
+     * post:
+     */
+    Celula* obtenerCelula();
+
+    /*
+     * pre:
+     * post:destuye la tableroCelda
+     */
+    ~Celda();
+
+    /*
+     * pre:
+     * post:
+     */
+    int getPosX();
+    /*
+     * pre:
+     * post:
+     */
+    int getPosY();
+    /*
+     * pre:
+     * post:
+     */
+    int getPosZ();
+
+    void agregarAdyacente(Celda *pCelda);
+    /*
+     * pre:
+     * post:
+     */
+    Lista<Celda*>* getCeldasAdyacentes();
+
+    void imprimirCelda();
+
+    void revivirCelula();
+    bool tieneEstadoProcreadora();
+    /*
+     * pre:
+     * post:
+     */
+    int obtenerCantCelVivasAdy();
+
+    int getEstado();
+
+    bool tieneEstadoContaminada();
+
+    void setNuevoEstado();
 };
 
 
-//estado qu epuede variar
+//estadoActual qu epuede variar
 /*
-int contaminada;//estado = 1:celda contaminada
-int envenenada;//estado = 2 :celda envenenada
-int procreadora;//estade = 3:celda preocreadora
-int portal;//estado = 4:celda portal
-//estado = 5 :celda radioactiva
+int contaminada;//estadoActual = 1:tableroCelda contaminada
+int envenenada;//estadoActual = 2 :tableroCelda envenenada
+int procreadora;//estade = 3:tableroCelda preocreadora
+int portal;//estadoActual = 4:tableroCelda portal
+//estadoActual = 5 :tableroCelda radioactiva
 int estadoDeCelda;
 */
 
