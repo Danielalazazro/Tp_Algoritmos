1250648b75a33e754cf2ee14773b6f22eea3b460
Danielalazazro <113920273+Danielalazazro@users.noreply.github.com>
2022-11-12 22:01:05 -0300

    Update Gen.cpp

--- Gen.cpp
+++ Gen.cpp
@@ -1,21 +1,20 @@
 //
-// Created by daniela on 01/10/22.
-//
-
 #include "Gen.h"
 #include <iostream>
-Gen::Gen() {}
+using namespace std;
+Gen::Gen() {
+    cargaGenetica = 0;
+}
 Gen::Gen(unsigned int cargaGen) {
     verificarEnRango(cargaGen);
     if (verificarEnRango(cargaGen)){
         this->cargaGenetica = cargaGen;
     }
     else {
-        cargaGenetica =  rand() % 255;
+       this->cargaGenetica =  rand() % 255;
     }
 }
-Gen::~Gen() {}
-
+Gen::~Gen(){}
 unsigned int Gen::getCargaGenetica() {
     return this->cargaGenetica;
 }
@@ -24,4 +23,28 @@ void Gen::setCargaGenetica(unsigned int cargaGenetica) {
 }
 bool Gen::verificarEnRango(unsigned int cargaGenetica) {
     return ((cargaGenetica >= 0) && (cargaGenetica <= maximaCargaGenetica));
+}
+void Gen::insertarCargaUsuario(){
+    unsigned int cargaGeneticaIngreso = 300;
+    cout << "Ingrese la  carga genetica  de la celula:"<< endl;
+    while(not (verificarEnRango(cargaGeneticaIngreso))){
+        cin >> cargaGeneticaIngreso;
+        if(not verificarEnRango(cargaGeneticaIngreso)){
+            cout << "Error, ingrese una carga genetica valida o dentro del rango"<<endl;
+        }
+    }
+    this->cargaGenetica = cargaGeneticaIngreso;
+}
+void Gen::insertarCargaAleatoria(){
+    int cargaGeneticaAleatoria = rand() % maximaCargaGenetica;
+    this->cargaGenetica = cargaGeneticaAleatoria;
+}
+bool Gen::estaVivoGen(){
+    {
+        return (cargaGenetica > 0);
+    }
+}
+void Gen::matarGen(){
+
+    cargaGenetica = 0;
 }
