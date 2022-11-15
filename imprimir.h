d22cb73445e519294db96c6eabf232be6e0740ac
MauroCCRS <90273222+MauroCCRS@users.noreply.github.com>
2022-11-15 15:55:32 -0300

    Update imprimir.h

--- imprimir.h
+++ imprimir.h
@@ -1,9 +1,4 @@
-/*
- * imprimir.h
- *
- *  Created on: 15/11/2022
- *      Author: algo2
- */
+
 
 #ifndef IMPRIMIR_H_
 #define IMPRIMIR_H_
@@ -13,15 +8,9 @@
 #include "Celula.h"
 
 
-
-
 void pintarCelda(BMP Imagen,const char* nombre,int profundidadActual);
 
-
 void imprimirTablero();
-
-
-
 
 
 #endif /* IMPRIMIR_H_ */
