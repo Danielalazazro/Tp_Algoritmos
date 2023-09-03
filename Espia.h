3b0e07cb6439c5cc1044c5b6e7463cbc5dcd5a50
Danielalazazro <113920273+Danielalazazro@users.noreply.github.com>
2023-09-03 14:58:12 -0300

    Create Espia.h

---
+++ Espia.h
@@ -0,0 +1,18 @@
+//
+// Created by daniela on 02/09/23.
+//
+
+#ifndef TP1_AUX__ESPIA_H
+#define TP1_AUX__ESPIA_H
+
+
+class Espia {
+private:
+    int fila;
+    int columna;
+public:
+    Espia(int fila, int columna);
+};
+
+
+#endif //TP1_AUX__ESPIA_H
